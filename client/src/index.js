0da47a7be1ee919ec4b923eecd58b98e16cf19fe
eloginovskikh <evglogino@gmail.com>
2022-03-24 13:18:07 +0200

    First commit

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+
+console.log("Working!");
+
+
+/**
+ *
+ * 1. http://sketchtoy.com/70318565
+ * 2. Creating HTML+CSS Mockup for our side (just static data, no fetching, no JS)
+ *
+ *
+ *
+ * 3. https://swapi.dev/ alternative: https://www.swapi.tech/
+ */
